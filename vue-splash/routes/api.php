89343a18172bdf8102e49a2f58bfced28ee983b7
kbys1025 <kbys1025@gmail.com>
2021-01-18 20:51:16 +0900

    ログインAPI

--- vue-splash/routes/api.php
+++ vue-splash/routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+Route::post('/login', 'Auth\LoginController@login')->name('login');
