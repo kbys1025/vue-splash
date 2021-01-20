2d9b05be7db15596002e7d58af6aca2fa7d8c8f0
kbys1025 <kbys1025@gmail.com>
2021-01-20 21:30:07 +0900

    Vuexの導入

--- vue-splash/resources/js/app.js
+++ vue-splash/resources/js/app.js
@@ -12,6 +12,8 @@ import router from './router'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
+import store from './store'
+
 
 /**
  * The following block of code may be used to automatically register your
@@ -35,6 +37,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
     router, // ルーティングの定義を読み込む
+    store,
     components: { App }, // ルートコンポーネントの使用を宣言する
     template: '<App />' // ルートコンポーネントを描画する
 });
